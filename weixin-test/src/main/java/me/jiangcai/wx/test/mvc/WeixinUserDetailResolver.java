f2a7844b32b854143e0b8675d729673d1d2cbf76
Cai Jiang <luffy.ja@gmail.com>
2017-05-14 23:42:27 +0800

    让测试的几个模块行为更声明更贴近；同时保持跟实际协议的脱离

--- weixin-test/src/main/java/me/jiangcai/wx/test/mvc/WeixinUserDetailResolver.java
+++ weixin-test/src/main/java/me/jiangcai/wx/test/mvc/WeixinUserDetailResolver.java
@@ -3,6 +3,8 @@ package me.jiangcai.wx.test.mvc;
 import me.jiangcai.wx.couple.WeixinRequestHandlerMapping;
 import me.jiangcai.wx.model.PublicAccount;
 import me.jiangcai.wx.model.WeixinUserDetail;
+import me.jiangcai.wx.standard.entity.StandardWeixinUser;
+import me.jiangcai.wx.standard.repository.StandardWeixinUserRepository;
 import me.jiangcai.wx.test.WeixinUserMocker;
 import me.jiangcai.wx.web.exception.NoWeixinClientException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,12 @@ import javax.servlet.http.HttpSession;
  */
 public class WeixinUserDetailResolver extends me.jiangcai.wx.web.mvc.WeixinUserDetailResolver {
 
+    private static final String DetailKey = "m.j.w.detail";
     private final WeixinUserMocker mocker;
     @Autowired
     private WeixinRequestHandlerMapping mapping;
+    @Autowired(required = false)
+    private StandardWeixinUserRepository standardWeixinUserRepository;
 
     public WeixinUserDetailResolver(WeixinUserMocker mocker) {
         super();
@@ -39,17 +44,42 @@ public class WeixinUserDetailResolver extends me.jiangcai.wx.web.mvc.WeixinUserD
         HttpSession session = webRequest.getNativeRequest(HttpServletRequest.class).getSession();
 
         if (session != null) {
-            String openId = (String) session.getAttribute(SK_Prefix_OpenID + account.getAppID());
-            if (!StringUtils.isEmpty(openId)) {
-                WeixinUserDetail endValue = weixinUserService.userInfo(account, openId, WeixinUserDetail.class, webRequest);
-                if (endValue != null)
-                    return endValue;
+            WeixinUserDetail detail = (WeixinUserDetail) session.getAttribute(DetailKey + account.getAppID());
+            if (detail != null) {
+                String openId = (String) session.getAttribute(SK_Prefix_OpenID + account.getAppID());
+                if (!StringUtils.isEmpty(openId))
+                    detail.setOpenId(openId);
+                else
+                    session.setAttribute(SK_Prefix_OpenID + account.getAppID(), detail.getOpenId());
+                return saveDetail(detail);
             }
+//            if (!StringUtils.isEmpty(openId)) {
+//                WeixinUserDetail endValue = weixinUserService.userInfo(account, openId, WeixinUserDetail.class, webRequest);
+//                if (endValue != null)
+//                    return endValue;
+//            }
         }
         final WeixinUserDetail weixinUserDetail = mocker.mockUser(mavContainer, webRequest);
         if (session != null) {
-            session.setAttribute(SK_Prefix_OpenID + account.getAppID(), weixinUserDetail.getOpenId());
+            session.setAttribute(DetailKey + account.getAppID(), weixinUserDetail);
+            String openId = (String) session.getAttribute(SK_Prefix_OpenID + account.getAppID());
+            if (!StringUtils.isEmpty(openId))
+                weixinUserDetail.setOpenId(openId);
+            else
+                session.setAttribute(SK_Prefix_OpenID + account.getAppID(), weixinUserDetail.getOpenId());
         }
-        return weixinUserDetail;
+        return saveDetail(weixinUserDetail);
+    }
+
+    private Object saveDetail(WeixinUserDetail detail) {
+        // 如果采用了标准方案 则应当自动储存
+        if (standardWeixinUserRepository == null)
+            return detail;
+        StandardWeixinUser user = standardWeixinUserRepository.findByOpenId(detail.getOpenId());
+        if (user != null)
+            return user;
+        user = new StandardWeixinUser();
+        WeixinUserDetail.Copy(user, detail);
+        return standardWeixinUserRepository.save(user);
     }
 }
